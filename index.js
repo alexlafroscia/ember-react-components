a9a4b01edbdbbd95a295c8639f2d1b44f482c467
Alex LaFroscia <alex@lafroscia.com>
2018-03-23 15:04:26 -0700

    Add and configure necessary babel transforms

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-react'
+  name: 'ember-cli-react',
+
+  appOptions() {
+    return (
+      (this.parent && this.parent.options) || (this.app && this.app.options)
+    );
+  },
+
+  included() {
+    this._super.included.apply(this, arguments);
+
+    const opts = this.appOptions();
+    opts.babel = opts.babel || {};
+    opts.babel.plugins = opts.babel.plugins || [];
+
+    opts.babel.plugins.push(['transform-class-properties', 'transform-react-jsx']);
+  }
 };
