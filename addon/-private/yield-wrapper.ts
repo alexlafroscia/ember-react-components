9c50f393252373bd7ae513b6369c2729739f6aab
Dan Freeman <dfreeman@salsify.com>
2018-06-10 11:52:36 -0400

    fix: be explicit about exported types so declaration emission works

--- addon/-private/yield-wrapper.ts
+++ addon/-private/yield-wrapper.ts
@@ -24,7 +24,7 @@ export default class YieldWrapper extends ReactComponent<YieldWrapperProps> {
     element.parentNode!.replaceChild(fragment, element);
   }
 
-  render() {
+  render(): any {
     // This element is temporary. When this is mounted,
     // it will be replaced by the children nodes, handled by Ember.
     return createElement('span', {
