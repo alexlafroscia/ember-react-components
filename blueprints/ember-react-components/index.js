796eb7f674a45edf9bcd5df69fa7e58a03ea40cb
Alex LaFroscia <alex@lafroscia.com>
2018-06-14 17:10:11 -0700

    chore: update React and ReactDOM versions

--- blueprints/ember-react-components/index.js
+++ blueprints/ember-react-components/index.js
@@ -1,26 +1,14 @@
 /* eslint-env node */
 
-const pkg = require('../../package.json');
-
-function getDependencyVersion(packageJson, name) {
-  var dependencies = packageJson.dependencies;
-  var devDependencies = packageJson.devDependencies;
-
-  return dependencies[name] || devDependencies[name];
-}
-
 module.exports = {
   description: 'Install React and ReactDOM',
 
   normalizeEntityName() {},
 
   afterInstall() {
-    return Promise.all([
-      this.addPackageToProject('react', getDependencyVersion(pkg, 'react')),
-      this.addPackageToProject(
-        'react-dom',
-        getDependencyVersion(pkg, 'react-dom')
-      )
+    return this.addPackagesToProject([
+      { name: 'react' },
+      { name: 'react-dom' }
     ]);
   }
 };
