4e710c6363ed04f02198ebbe44f2f4e6ab8fb216
Alex LaFroscia <alex@lafroscia.com>
2019-01-31 21:52:10 -0800

    fix: install React, React DOM through `ember-auto-import`
    This will hopefully solve some issues with using third-party React components
    
    Related to #23

--- index.js
+++ index.js
@@ -5,40 +5,8 @@ const configureJsxTransform = require('./lib/configure-jsx-transform');
 module.exports = {
   name: 'ember-react-components',
 
-  appOptions() {
-    return (
-      (this.parent && this.parent.options) || (this.app && this.app.options)
-    );
-  },
-
   included(parent) {
     this._super.included.apply(this, arguments);
-
-    const opts = this.appOptions();
-    const cliOpts = opts['ember-react-components'];
-    const outputFile = (cliOpts && cliOpts.outputFile) || '/assets/vendor.js';
-
-    this.import(
-      {
-        development: 'node_modules/react/umd/react.development.js',
-        production: 'node_modules/react/umd/react.production.min.js'
-      },
-      {
-        using: [{ transformation: 'amd', as: 'react' }],
-        outputFile
-      }
-    );
-
-    this.import(
-      {
-        development: 'node_modules/react-dom/umd/react-dom.development.js',
-        production: 'node_modules/react-dom/umd/react-dom.production.min.js'
-      },
-      {
-        using: [{ transformation: 'amd', as: 'react-dom' }],
-        outputFile
-      }
-    );
 
     configureJsxTransform(parent);
   }
