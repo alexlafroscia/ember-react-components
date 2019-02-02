da1c5dedccf2f6ea16e5624bf1be5e739fd29dd6
Alex LaFroscia <alex@lafroscia.com>
2019-02-02 00:24:03 -0800

    chore(deps): run `ember-cli-update` to `3.7.1`

--- config/ember-try.js
+++ config/ember-try.js
@@ -13,9 +13,23 @@ module.exports = function() {
       scenarios: [
         {
           name: 'ember-lts-2.18',
+          env: {
+            EMBER_OPTIONAL_FEATURES: JSON.stringify({
+              'jquery-integration': true
+            })
+          },
           npm: {
             devDependencies: {
+              '@ember/jquery': '^0.5.1',
               'ember-source': '~2.18.0'
+            }
+          }
+        },
+        {
+          name: 'ember-lts-3.4',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.4.0'
             }
           }
         },
@@ -49,6 +63,16 @@ module.exports = function() {
             dependencies: {
               'ember-cli-babel': '^6.6.0'
             }
+          }
+        },
+        // The default `.travis.yml` runs this scenario via `npm test`,
+        // not via `ember try`. It's still included here so that running
+        // `ember try:each` manually or from a customized CI config will run it
+        // along with all the other scenarios.
+        {
+          name: 'ember-default',
+          npm: {
+            devDependencies: {}
           }
         }
       ]
