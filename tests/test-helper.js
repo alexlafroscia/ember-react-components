1735f95f42c5203838f0836d4d1459d22d567858
Shane Martin <shane.martin@rewardops.com>
2021-06-09 16:06:11 -0400

    test: simply test setup

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -3,10 +3,6 @@ import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 import setupSinon from 'ember-sinon-qunit';
-import { setup } from 'qunit-dom';
-import QUnit from 'qunit';
-
-setup(QUnit.assert);
 
 setApplication(Application.create(config.APP));
 
