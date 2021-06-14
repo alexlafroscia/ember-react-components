6a8e0f8f992389d857eaeece5f7268e4bdbc00d8
Shane Martin <shane.martin@rewardops.com>
2021-06-14 17:13:54 -0400

    test: reorganize test mocks

--- tests/unit/-private/is-function-component-test.js
+++ tests/unit/-private/is-function-component-test.js
@@ -4,21 +4,21 @@ import React from 'react';
 
 import isFunctionalComponent from 'ember-react-components/-private/is-functional-component';
 
-const MockReactFunctionalCcomponent = () =>
-  React.createElement('div', null, 'Foo');
-
-class MockReactClassComponent extends React.Component {
-  render() {
-    <div>Bar</div>;
-  }
-}
-
 module('Unit | Private Utility | isReactComponent', function () {
   test('it can detect React.FC component', function (assert) {
-    assert.ok(isFunctionalComponent(MockReactFunctionalCcomponent));
+    const MockReactFunctionalComponent = () =>
+      React.createElement('div', null, 'Foo');
+
+    assert.ok(isFunctionalComponent(MockReactFunctionalComponent));
   });
 
   test('it can detect non-React.FC component', function (assert) {
+    class MockReactClassComponent extends React.Component {
+      render() {
+        <div>Bar</div>;
+      }
+    }
+
     assert.notOk(isFunctionalComponent(MockReactClassComponent));
   });
 });
